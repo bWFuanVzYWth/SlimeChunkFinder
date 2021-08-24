0f4cbaa6e916c5a73695f584afe5b6c04b11a107
linlin <52383012+origin0110@users.noreply.github.com>
2021-08-24 18:00:38 +0800

    Add files via upload

--- slimechunkfinder.c
+++ slimechunkfinder.c
@@ -1,4 +1,65 @@
-#include"slimechunkfinder.h"
+
+#include<stdint.h>
+#include<stdio.h>
+
+#define FC "slimechunk.bin" //file name of cache
+#define L 0x20000000 //bytes used by the cache
+
+#define FO "slimechunk.txt" //file name of output
+#define W 0x800000 //max valid chunk position
+
+	uint32_t get_seed(int32_t, int32_t);
+	uint8_t is_slime_chunk(uint32_t);
+	void write_cache(uint32_t, uint32_t);
+	uint8_t read_cache(uint32_t);
+
+	uint8_t c[L] = { 0 };
+
+void test(void) {
+	for (int z = -10; z < 10; z++) {
+		for (int x = -10; x < 10; x++) {
+			uint8_t b = read_cache(get_seed(x, z));
+			putchar(b ? '#' : ' ');
+			putchar(' ');
+		}
+		putchar('\n');
+	}
+}
+
+int main(void) {
+	FILE* fp = NULL;
+	fp = fopen(FC, "rb");
+	if (fp == NULL) {
+		printf("%s no found. Trying to regenerate.\n", FC);
+		printf("Please wait a few minutes.\n");
+		write_cache(0, L);
+		fp = fopen(FC, "wb");
+		if (fp == NULL) {
+			printf("Can not write %s\n", FC);
+			goto error;
+		}
+		fwrite(c, 1, L, fp);
+		fclose(fp);
+		printf("%s has been regenerated.\n", FC);
+	}
+	fp = fopen(FC, "rb");
+	if (fp == NULL) {
+		printf("Can not read %s\n", FC);
+		goto error;
+	}
+
+	fread(c, 1, L, fp);
+	fclose(fp);
+
+	test();
+
+	return 0;
+
+error:
+	printf("There seems to be something wrong :(\n");
+	return 1;
+
+}
 
 uint8_t is_slime_chunk(uint32_t s)
 {
