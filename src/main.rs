1ca1571c447b4e83e1f35767b158b936352220fd
linlin <52383012+bWFuanVzYWth@users.noreply.github.com>
2025-09-19 19:29:35 +0800

    封装lut相关数据

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,21 @@
 #![feature(portable_simd)]
-#![deny(
-clippy::unwrap_used,           // 禁止使用 `unwrap`
-clippy::expect_used,           // 禁止使用 `expect`
-clippy::panic_in_result_fn,    // 禁止在返回 `Result` 的函数中调用 `panic!`
-clippy::todo,                  // 禁止使用 `todo!`
-clippy::unreachable,           // 禁止使用 `unreachable!`
-clippy::unimplemented,          // 禁止使用 `unimplemented!`
 
-clippy::shadow_unrelated,
-clippy::shadow_same,
-)]
-#![warn(
-clippy::pedantic,              // 启用所有严格的 Lint
-clippy::cargo,                 // 启用与 Cargo 相关的 Lint
-clippy::nursery                // 启用实验性的 Lint
-)]
+// #![warn(
+// clippy::unwrap_used,           // 禁止使用 `unwrap`
+// clippy::expect_used,           // 禁止使用 `expect`
+// clippy::panic_in_result_fn,    // 禁止在返回 `Result` 的函数中调用 `panic!`
+// clippy::todo,                  // 禁止使用 `todo!`
+// clippy::unreachable,           // 禁止使用 `unreachable!`
+// clippy::unimplemented,          // 禁止使用 `unimplemented!`
+
+// clippy::shadow_unrelated,
+// clippy::shadow_same,
+// )]
+// #![warn(
+// clippy::pedantic,              // 启用所有严格的 Lint
+// clippy::cargo,                 // 启用与 Cargo 相关的 Lint
+// clippy::nursery                // 启用实验性的 Lint
+// )]
 
 use std::time::Instant;
 mod slime_chunk;
@@ -22,7 +23,7 @@ mod slime_chunk;
 fn main() {
     // 初始化：生成/读取lut
     let init_timer_start = Instant::now();
-    let lut = slime_chunk::init_lut();
+    let lut = slime_chunk::SlimeChunkLut::new();
     let init_timer = init_timer_start.elapsed().as_micros();
     println!("初始化耗时：{}s", (init_timer as f64) / 1000000.0);
 
@@ -31,7 +32,7 @@ fn main() {
         for x in -10..10 {
             print!(
                 "{}",
-                if slime_chunk::is_slime_chunk(&lut, slime_chunk::get_seed(x, z)) {
+                if lut.is_slime_chunk(slime_chunk::get_seed(x, z)) {
                     "# "
                 } else {
                     ". "
--- src/slime_chunk.rs
+++ src/slime_chunk.rs
@@ -9,65 +9,73 @@ const BLOCK_BITWISE: usize = 2048;
 const BLOCK_LENGTH: usize = BLOCK_BITWISE / SEED_BITWISE;
 const LUT_LENGTH: usize = (u32::MAX as usize + 1) / SEED_BITWISE;
 
-// 分块：多线程
-pub fn init_lut() -> Vec<u32> {
-    (0..(LUT_LENGTH / BLOCK_LENGTH) as u32)
-        .into_par_iter()
-        .map(|chunk_i| {
-            let seed_base = chunk_i * (SEED_BITWISE * BLOCK_LENGTH) as u32;
-            process_chunk(seed_base)
-        })
-        .collect::<Vec<_>>()
-        .concat()
+pub struct SlimeChunkLut {
+    lut: Box<[u32; LUT_LENGTH]>,
 }
 
-// 分组：simd
-fn process_chunk(seed_base: u32) -> [u32; BLOCK_LENGTH] {
-    const M: u32 = 397;
-    const MATRIX_A: u32 = 0x9908_b0df;
-    const UPPER_MASK: u32 = 0x8000_0000;
-    const LOWER_MASK: u32 = 0x7fff_ffff;
-
-    let mut result = u32x64::splat(0);
-
-    for offset in 0..(SEED_BITWISE as u32) {
-        let seed = u32x64::from_array(array::from_fn(|i| {
-            seed_base + offset + (i as u32) * (SEED_BITWISE as u32)
-        }));
-
-        // 初始化丐版mt19937的内部状态
-        let mut m = seed;
-        let m0 = m;
-        m = u32x64::splat(1_812_433_253) * (m ^ m >> 30) + u32x64::splat(1);
-        let m1 = m;
-        for i in 2..=M {
-            m = u32x64::splat(1_812_433_253) * (m ^ m >> 30) + u32x64::splat(i);
+impl SlimeChunkLut {
+    pub fn new() -> Self {
+        Self {
+            lut: (0..(LUT_LENGTH / BLOCK_LENGTH) as u32)
+                .into_par_iter()
+                .map(|chunk_i| {
+                    let seed_base = chunk_i * (SEED_BITWISE * BLOCK_LENGTH) as u32;
+                    Self::process_block(seed_base)
+                })
+                .collect::<Vec<_>>()
+                .concat()
+                .try_into()
+                .unwrap(),
         }
-        let mm = m;
-
-        // 生成一个伪随机数
-        let mut y = (m0 & u32x64::splat(UPPER_MASK)) | (m1 & u32x64::splat(LOWER_MASK));
-        let y_mask = !((y & u32x64::splat(1)) - u32x64::splat(1));
-        y = mm ^ (y >> 1) ^ (y_mask & u32x64::splat(MATRIX_A));
-
-        y ^= y >> 11;
-        y ^= y << 7 & u32x64::splat(0x9d2c_5680);
-        y ^= y << 15 & u32x64::splat(0xefc6_0000);
-        y ^= y >> 18;
-
-        // 判断是否是10的整数倍
-        let mut is_slime_chunk = y % u32x64::splat(10);
-        is_slime_chunk = (is_slime_chunk - u32x64::splat(1)) >> (SEED_BITWISE - 1) as u32;
-
-        // 压位
-        result |= is_slime_chunk << u32x64::splat(offset);
     }
 
-    result.to_array()
-}
+    fn process_block(seed_base: u32) -> [u32; BLOCK_LENGTH] {
+        const M: u32 = 397;
+        const MATRIX_A: u32 = 0x9908_b0df;
+        const UPPER_MASK: u32 = 0x8000_0000;
+        const LOWER_MASK: u32 = 0x7fff_ffff;
 
-pub const fn is_slime_chunk(lut: &[u32], seed: u32) -> bool {
-    lut[seed as usize / SEED_BITWISE] & (1 << (seed % SEED_BITWISE as u32)) != 0
+        let mut result = u32x64::splat(0);
+
+        for offset in 0..(SEED_BITWISE as u32) {
+            let seed = u32x64::from_array(array::from_fn(|i| {
+                seed_base + offset + (i as u32) * (SEED_BITWISE as u32)
+            }));
+
+            // 初始化丐版mt19937的内部状态
+            let mut m = seed;
+            let m0 = m;
+            m = u32x64::splat(1_812_433_253) * (m ^ m >> 30) + u32x64::splat(1);
+            let m1 = m;
+            for i in 2..=M {
+                m = u32x64::splat(1_812_433_253) * (m ^ m >> 30) + u32x64::splat(i);
+            }
+            let mm = m;
+
+            // 生成一个伪随机数
+            let mut y = (m0 & u32x64::splat(UPPER_MASK)) | (m1 & u32x64::splat(LOWER_MASK));
+            let y_mask = !((y & u32x64::splat(1)) - u32x64::splat(1));
+            y = mm ^ (y >> 1) ^ (y_mask & u32x64::splat(MATRIX_A));
+
+            y ^= y >> 11;
+            y ^= y << 7 & u32x64::splat(0x9d2c_5680);
+            y ^= y << 15 & u32x64::splat(0xefc6_0000);
+            y ^= y >> 18;
+
+            // 判断是否是10的整数倍
+            let mut is_slime_chunk = y % u32x64::splat(10);
+            is_slime_chunk = (is_slime_chunk - u32x64::splat(1)) >> (SEED_BITWISE - 1) as u32;
+
+            // 压位
+            result |= is_slime_chunk << u32x64::splat(offset);
+        }
+
+        result.to_array()
+    }
+
+    pub const fn is_slime_chunk(&self, seed: u32) -> bool {
+        self.lut[seed as usize / SEED_BITWISE] & (1 << (seed % SEED_BITWISE as u32)) != 0
+    }
 }
 
 pub const fn get_seed(x: i32, z: i32) -> u32 {
